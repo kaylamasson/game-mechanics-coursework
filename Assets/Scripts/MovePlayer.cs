e8f886089985aaf361f2ed2cda051d8fc22c6cbd
kayla <kayla.masson05@gmail.com>
2024-10-20 00:57:06 +0100

    walking animation

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -10,6 +10,15 @@ public class MovePlayer : MonoBehaviour
 
     public string left;
     public string right; 
+    public string jump; 
+
+    private bool isFacingRight = true; 
+
+    private float horizontalInput; 
+
+
+    public Animator animator; 
+    public bool isJumping = false; 
 
 
     public Rigidbody2D rb; 
@@ -18,26 +27,67 @@ public class MovePlayer : MonoBehaviour
     {
 
         rb = this.GetComponent<Rigidbody2D>();
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontalInput);
+
         Vector3 move = new Vector3(0,0,0);
+
+        if (horizontalInput != 0)
+        {
+            animator.SetBool("isWalking",true);
+        } else 
+        {
+            animator.SetBool("isWalking",false);
+        }
 
         if (Input.GetKey(left))
         {
-            move = new Vector3(-0.01f, 0, 0);
+            move = new Vector3(-0.02f, 0, 0);
         }
 
         if (Input.GetKey(right))
         {
-            move = new Vector3(0.01f, 0, 0);
+            move = new Vector3(0.02f, 0, 0);
+        }
+
+        if (Input.GetKey(jump) && isJumping == false)
+        {
+
+            animator.SetBool("isWalking",false);
+            isJumping = true;
+            rb.AddForce(this.transform.up * 1500);
         }
 
         this.transform.Translate(move);
         
+        FlipSprite();
+    }
+
+
+    void FlipSprite()
+    {
+
+        if ((isFacingRight && horizontalInput < 0f) || (!isFacingRight && horizontalInput > 0f))
+        {
+
+            isFacingRight = !isFacingRight;
+            Vector3 ls = this.transform.localScale;
+            ls.x *= -1;
+            this.transform.localScale = ls;
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
     }
 }
